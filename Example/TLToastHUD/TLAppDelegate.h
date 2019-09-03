405100b7d80c5d7aa5a948785ba55141130d2215
huawt <ghost263sky@163.com>
2019-09-03 15:09:22 +0800

    Initial commit

---
+++ Example/TLToastHUD/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  TLToastHUD
+//
+//  Created by huawt on 09/03/2019.
+//  Copyright (c) 2019 huawt. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/TLToastHUD/TLViewController.h
@@ -0,0 +1,13 @@
+//
+//  TLViewController.h
+//  TLToastHUD
+//
+//  Created by huawt on 09/03/2019.
+//  Copyright (c) 2019 huawt. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TLViewController : UIViewController
+
+@end
